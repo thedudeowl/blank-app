79baca2280d99c25052e3854d712fe05244434f0
thedudeowl <asualum2@gmail.com>
2026-02-27 14:08:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 @st.cache_data
 def load_data(nrows): 
   data = pd.read_csv(DATA_URL, nrows=nrows) 
+  
 lowercase = lambda x: str(x).lower() 
 data.rename(lowercase, axis='columns', inplace=True) 
 data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN]) 
