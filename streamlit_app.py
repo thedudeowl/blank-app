af0f27c8cbf0280b2f9ccedb35ad857c78207004
thedudeowl <asualum2@gmail.com>
2026-02-27 14:08:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def load_data(nrows):
 lowercase = lambda x: str(x).lower() 
 data.rename(lowercase, axis='columns', inplace=True) 
 data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN]) 
-return data 
+  return data 
 
 # Create a text element and let the reader know the data is loading. 
 data_load_state = st.text('Loading data...') 
