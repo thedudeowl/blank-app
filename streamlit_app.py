fc9f26756bd9bddba3f67bb91207aa3520ccfd9e
thedudeowl <asualum2@gmail.com>
2026-02-27 13:36:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,6 @@ data = load_data(10000)
 
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
+
+st.subheader('Raw data')
+st.write(data)
